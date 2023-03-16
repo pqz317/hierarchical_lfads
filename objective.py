687144fd0e15468161df8258923f36f83270b7b0
Patrick Zhang <pqz317@uw.edu>
2023-03-16 19:25:31 +0000

    to devices, debugging

--- objective.py
+++ objective.py
@@ -160,6 +160,8 @@ class LogLikelihoodPoisson(nn.Module):
         
     def forward(self, k, lam):
 #         pdb.set_trace()
+        print(k.device)
+        print(lam.device)
         return loglikelihood_poisson(k, lam*self.dt)
 
 class LogLikelihoodPoissonSimple(nn.Module):
